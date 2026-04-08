5ba1d3ce6b51c73648b8c9143d80a11d85d9996e
Daniil Nikishyn <daniil.nikishyn@gmail.com>
2026-04-08 03:09:48 +0300

    Refactor Telegram bot configuration and message processing logic

--- src/main/java/io/github/aihio/bot/MessageProcessor.java
+++ src/main/java/io/github/aihio/bot/MessageProcessor.java
@@ -1,19 +1,12 @@
 package io.github.aihio.bot;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.telegram.model.OutgoingTextMessage;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-@ApplicationScoped
 public class MessageProcessor extends RouteBuilder {
-
-    @ConfigProperty(name = "telegram.token")
-    String token;
-
     @Override
     public void configure() {
-        from("telegram:bots?authorizationToken=" + token)
+        from("telegram:bots")
                 .log("${body}")
                 .process(exchange -> {
                     var incoming = exchange.getMessage().getBody(String.class);
@@ -21,7 +14,6 @@ public class MessageProcessor extends RouteBuilder {
                     msg.setText(incoming);
                     exchange.getIn().setBody(msg);
                 })
-                .to("telegram:bots?authorizationToken=" + token);
-
+                .to("telegram:bots");
     }
 }
